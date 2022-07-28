59ef25ef0137fd647a3eda8c85b9934ab41651b7
Chanchal Choudhary <Chanchal.Choudhary@fcsamerica.com>
2022-07-28 13:53:55 -0500

    reducers initial

--- src/app/auth/reducers/index.ts
+++ src/app/auth/reducers/index.ts
@@ -2,20 +2,36 @@ import {
   ActionReducer,
   ActionReducerMap,
   createFeatureSelector,
+  createReducer,
   createSelector,
-  MetaReducer
-} from '@ngrx/store';
+  MetaReducer,
+  on,
+} from "@ngrx/store";
+import { User } from "../model/user.model";
+import { AuthActions } from "../action-types";
 
+export const authFeatureKey = "auth";
 
-export const authFeatureKey = 'auth';
-
-export interface State {
-
+export interface AuthState {
+  user: User;
 }
 
-export const reducers: ActionReducerMap<State> = {
-
+export const initialAuthState: AuthState = {
+  user: undefined,
 };
 
+export const authReducer = createReducer(
+  initialAuthState,
 
+  on(AuthActions.login, (state, action) => {
+    return {
+      user: action.user,
+    };
+  }),
 
+  on(AuthActions.logout, (state, action) => {
+    return {
+      user: undefined,
+    };
+  })
+);

d573f7109addd176c516e70fa0e968bb34a6ae10
Chanchal Choudhary <Chanchal.Choudhary@fcsamerica.com>
2022-07-28 10:57:27 -0500

    added more actions

--- src/app/auth/auth.actions.ts
+++ src/app/auth/auth.actions.ts
@@ -3,3 +3,8 @@ import { User } from "./model/user.model";
 
 export const login = createAction("[Login Page] User Login", props<{ user: User }>());
 
+export const logout = createAction(
+  "[Top Menu] Logout",
+  props<{ user: User }>()
+);
+
